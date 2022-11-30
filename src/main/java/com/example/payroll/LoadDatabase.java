d716c84f55b78bbe3187cbbd0739d310e6a6716a
Niklas <niklas.nebeling@klosebrothers.de>
2022-11-30 10:03:05 +0100

    initial commit

---
+++ src/main/java/com/example/payroll/LoadDatabase.java
@@ -0,0 +1,24 @@
+package com.example.payroll;
+
+import com.example.payroll.entities.Employee;
+import com.example.payroll.repositories.EmployeeRepository;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.slf4j.Logger;
+
+@Configuration
+public class LoadDatabase {
+
+    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+    @Bean
+    CommandLineRunner initDatabase(EmployeeRepository repository) {
+        return args -> {
+            log.info("Preloading " + repository.save(new Employee("Bilbo", "Baggins", "burglar")));
+            log.info("Preloading " + repository.save(new Employee("Frodo", "Baggins", "thief")));
+        };
+    }
+}
---
+++ src/main/java/com/example/payroll/errorhandling/EmployeeNotFoundAdvice.java
@@ -0,0 +1,16 @@
+package com.example.payroll.errorhandling;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+public class EmployeeNotFoundAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(EmployeeNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String employeeNotFoundHandler(EmployeeNotFoundException exception) {
+        return exception.getMessage();
+    }
+}
